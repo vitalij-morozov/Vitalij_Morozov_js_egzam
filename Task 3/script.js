b560dd23473f46495bdafc41e6000a79b98bd19b
Vitalij <vitaliy.morozov1@gmail.com>
2022-08-18 10:24:26 +0300

    task-1 responsiveness fix, task-3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,55 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+// Nusitaikymai
+const outputContainerEl = document.getElementById("output");
+const btnEl = document.getElementById("btn");
+
+// Pagrindine nuoroda
+const ENDPOINT = "https://api.github.com/users";
+
+// GET metodas, gaunamas useriu masyvas is API
+async function getUsers(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Fetch GET failed", response.status, response.message);
+    }
+    const dataInJs = await response.json();
+    console.log("GET dataInJs ===", dataInJs);
+    return dataInJs;
+  } catch (error) {
+    console.warn(error.message);
+  }
+}
+
+// Mygtugo Show Users Event Listenter, paspaudus Useriai gaunami is API ir atvaizduojami HTML
+btnEl.addEventListener("click", () => {
+  getUsers(ENDPOINT)
+    .then(displayUsersInHTML)
+    .catch((error) => console.warn(error));
+});
+
+// Sukuriami User korteles HTML elementai
+function createUserCard(uObj) {
+  const divEl = document.createElement("div");
+  divEl.className = "user-card";
+  const imgEl = document.createElement("img");
+  imgEl.src = uObj.avatar_url;
+  imgEl.className = "user-card__avatar";
+  const h2El = document.createElement("h2");
+  h2El.className = "user-card__username";
+  h2El.textContent = uObj.login;
+
+  divEl.append(imgEl, h2El);
+  return divEl;
+}
+
+// Gauti Useriai atvaizduojami HTML
+function displayUsersInHTML(uArr) {
+  outputContainerEl.innerHTML = "";
+  const users = uArr.map((uObj) => {
+    return createUserCard(uObj);
+  });
+  outputContainerEl.append(...users);
+}
