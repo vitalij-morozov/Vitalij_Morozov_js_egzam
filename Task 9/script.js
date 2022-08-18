842739ccaf84f20add1d5b94ea1ce29252dbcf7c
Vitalij <vitaliy.morozov1@gmail.com>
2022-08-18 11:52:41 +0300

    task-9 completed

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,24 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  /**
+   * constructor funkcija sukuria objekta su zemiau pateiktais parametrais
+   * @param {string} title
+   * @param {string} director
+   * @param {number} budget
+   */
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  get wasExpensive() {
+    const bigBudget = 100000000;
+    return this.budget > bigBudget ? true : false;
+  }
+}
+
+const filmas = new Movie("Titanic", "James Bond", 200000000);
+console.log("filmas.wasExpensive ===", filmas.wasExpensive);
